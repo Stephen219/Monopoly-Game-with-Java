22e58fc128805956e0b4005916a87eeae8e3bc80
Stephen Kariuki <kariukism1@cardiff.ac.uk>
2023-04-28 11:43:06 +0100

    add a player

--- src/main/java/com/cm6123/monopoly/app/Application.java
+++ src/main/java/com/cm6123/monopoly/app/Application.java
@@ -1,5 +1,4 @@
 package com.cm6123.monopoly.app;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- src/main/java/com/cm6123/monopoly/game/Player.java
+++ src/main/java/com/cm6123/monopoly/game/Player.java
@@ -3,4 +3,22 @@ package com.cm6123.monopoly.game;
  * this is a player class with all the methods and attributes needed to play the game.
  */
 public class Player {
+    /**
+     * Represents wheelbarrow the piece.
+     */
+    public static final int WHEELBARROW      = 0;
+    /**
+     * Represents battleship the piece.
+     */
+    public static final int BATTLESHIP       = 1;
+    /**
+     * Represents sack of money the piece.
+     */
+    public static final int SACK_OF_MONEY    = 2;
+    /**
+     * Represents horse and rider the piece.
+     */
+    public static final int HORSE_AND_RIDER  = 3;
+
+
 }
